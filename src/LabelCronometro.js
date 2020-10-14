2e2aa20a40b8450f89c285043e9acc874ce4cdc5
opablomonteiro <63929281+opablomonteiro@users.noreply.github.com>
2020-10-14 11:43:14 -0300

    base em teste

--- src/LabelCronometro.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './App.css'
-
-const LabelCronometro = (props) => (
-    <h1 className="my-title">{props.name}</h1>
-)
-
-export default LabelCronometro
--- src/LabelRelogio.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './App.css'
-
-const LabelRelogio = (props) => (
-    <h1 className="my-title">{props.name}</h1>
-)
-
-export default LabelRelogio
